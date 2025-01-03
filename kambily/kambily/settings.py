ce77a2be4253c0a5525c2060803675fe19494b12
koulibalyamadou10 <koulibalyamadou10@gmail.com>
2025-01-03 21:01:23 +0000

    ajout du nouveau lien

--- kambily/kambily/settings.py
+++ kambily/kambily/settings.py
@@ -163,7 +163,8 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://kambily-two.vercel.app"
+    "https://kambily-two.vercel.app",
+    "https://kambily-a.vercel.app"
 ]
 
 MEDIA_URL = '/media/'
